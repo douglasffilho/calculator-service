93188e670209a3997b85c8dd899ed6aa650712e9
Douglas Filho <douglasf.filho@gmail.com>
2024-08-30 15:44:36 -0300

    chore: update operations controller - [x] HTTP client layer to communicate with the integration - [x] HTTP call to random-string generation before default logic (only executes default logic in a circuit-break case as a fallback)

--- src/calculator_service/controller/operations.clj
+++ src/calculator_service/controller/operations.clj
@@ -1,5 +1,13 @@
 (ns calculator-service.controller.operations
-  (:require [calculator-service.logic.operations :as logic.operations]))
+  (:require [calculator-service.infra.http.client.random-string :as http.client.random-string]
+            [calculator-service.logic.operations :as logic.operations]))
 
 (defn execute [operation]
-  (logic.operations/execute operation))
+  (if (= (get operation :type) :random-string)
+    (let [response      (http.client.random-string/get!)
+          error?        (get response :error?)
+          random-string (get response :data)]
+      (if error?
+        (logic.operations/execute operation)
+        random-string))
+    (logic.operations/execute operation)))
